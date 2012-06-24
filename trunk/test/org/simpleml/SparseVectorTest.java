d8ef02ed3ccb0086f53a01d0c0dd486ddbdd28d9
rasmikun@gmail.com <rasmikun@gmail.com@4f855a97-4f9d-c04b-2fe2-69cd1aad2113>
2012-06-24 20:22:02 +0000

    SparseVector: Fix contructors

--- trunk/test/org/simpleml/SparseVectorTest.java
+++ trunk/test/org/simpleml/SparseVectorTest.java
@@ -3,20 +3,46 @@ package org.simpleml;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author rasmikun
  */
 public class SparseVectorTest {
     private Vector aVector = new ArrayVector(new double[]{1, 1, 1, 0, 0, 1});
-    private Vector sVector = new SparseVector(new double[]{1, 1, 1});
+    private Vector sVector;
 
     @Test
     public void testSetSparseVectorTest() throws Exception {
-        sVector.set(5, 1);
+        Map<Integer, Double> map = new HashMap<Integer, Double>();
+        map.put(0, 1d);
+        map.put(1, 1d);
+        map.put(2, 1d);
+        map.put(5, 1d);
+
+        sVector = new SparseVector(map);
 
         Assert.assertEquals(sVector.size(), aVector.size());
         for (int i = 0; i < aVector.size(); i++) {
             Assert.assertEquals(sVector.get(i), aVector.get(i));
+        }
+    }
+
+    @Test
+    public void testSetIteratorSparseVectorText() throws Exception {
+        ArrayList<IndexedValue> arrayList = new ArrayList<IndexedValue>();
+        arrayList.add(new IndexedValue(0, 1d));
+        arrayList.add(new IndexedValue(1, 1d));
+        arrayList.add(new IndexedValue(2, 1d));
+        arrayList.add(new IndexedValue(5, 1d));
+
+        Vector otherSparseVector = new SparseVector(arrayList.iterator());
+
+        Assert.assertEquals(otherSparseVector.size(), aVector.size());
+        for (int i = 0; i < aVector.size(); i++) {
+            Assert.assertEquals(aVector.get(i), otherSparseVector.get(i));
         }
     }
 
